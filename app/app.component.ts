12fdda374e379f709e15c0b1fe57a00cac4b394b
fasfsfgs <fasfsfgs@gmail.com>
2016-02-07 15:42:50 -0200

    beta: the hero editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>my angular 2 app</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <div><input type="text" [(ngModel)]="hero.name" placeholder="name"></div>
+        </div>
+    `
 })
 export class AppComponent {
+    public hero:Hero = {
+        id: 1,
+        name: 'windstorm'
+    };
+
+    public title = 'tour of heroes';
 }
