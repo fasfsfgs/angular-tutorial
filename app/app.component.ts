983988e7bb6771807859a7c4aecba4097b292352
fasfsfgs <fasfsfgs@gmail.com>
2016-02-07 16:24:32 -0200

    beta: master/detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,19 +9,91 @@ interface Hero {
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <div><input type="text" [(ngModel)]="hero.name" placeholder="name"></div>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ngFor="#hero of heroes"
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+                <label>name: </label>
+                <div><input type="text" [(ngModel)]="selectedHero.name" placeholder="name"></div>
+            </div>
         </div>
-    `
+    `,
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 10em;
+        }
+        .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0em;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+            color: white;
+        }
+        .heroes li:hover {
+            color: #607D8B;
+            background-color: #EEE;
+            left: .1em;
+        }
+        .heroes .text {
+            position: relative;
+            top: -3px;
+        }
+        .heroes .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0em 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0px 0px 4px;
+        }
+    `]
 })
 export class AppComponent {
-    public hero:Hero = {
-        id: 1,
-        name: 'windstorm'
-    };
-
     public title = 'tour of heroes';
+    public heroes = HEROES;
+    public selectedHero:Hero;
+
+    onSelect(hero:Hero) {
+        this.selectedHero = hero;
+    }
 }
+
+var HEROES:Hero[] = [
+    {"id": 11, "name": "mr. nice"},
+    {"id": 12, "name": "narco"},
+    {"id": 13, "name": "bombasto"},
+    {"id": 14, "name": "celeritas"},
+    {"id": 15, "name": "magneta"},
+    {"id": 16, "name": "rubberman"},
+    {"id": 17, "name": "dynama"},
+    {"id": 18, "name": "dr iq"},
+    {"id": 19, "name": "magma"},
+    {"id": 20, "name": "tornado"}
+];
