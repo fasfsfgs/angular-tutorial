09940c881de54518b0e700a65cc165bd0a4ac8eb
fasfsfgs <fasfsfgs@gmail.com>
2016-02-07 17:29:16 -0200

    beta: multiple components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component} from 'angular2/core';
-
-interface Hero {
-    id: number;
-    name: string;
-}
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -17,14 +14,7 @@ interface Hero {
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id: </label>{{selectedHero.id}}</div>
-            <div>
-                <label>name: </label>
-                <div><input type="text" [(ngModel)]="selectedHero.name" placeholder="name"></div>
-            </div>
-        </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
     styles: [`
         .selected {
@@ -73,7 +63,8 @@ interface Hero {
             margin-right: .8em;
             border-radius: 4px 0px 0px 4px;
         }
-    `]
+    `],
+    directives: [HeroDetailComponent]
 })
 export class AppComponent {
     public title = 'tour of heroes';
